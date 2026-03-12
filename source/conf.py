79e8bd1c0a0343c31b4a7870fbb4ab2438aecc59
zahirmahammad <zahirmd@umd.edu>
2026-03-12 00:33:57 -0700

    theme

--- source/conf.py
+++ source/conf.py
@@ -40,7 +40,8 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
 
 # --- Zahir ------
